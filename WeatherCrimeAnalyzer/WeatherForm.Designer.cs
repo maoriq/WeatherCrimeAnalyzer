d163730f549c2346645b3d2f23fa3a23879458a0
maoriq <angelika.chopik@mail.ru>
2025-05-30 17:17:46 +0300

    убрала лишнее

--- WeatherCrimeAnalyzer/WeatherForm.Designer.cs
+++ WeatherCrimeAnalyzer/WeatherForm.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.weatherGridView = new System.Windows.Forms.DataGridView();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.LoadWeather = new System.Windows.Forms.Button();
@@ -47,26 +47,26 @@
             // weatherGridView
             // 
             this.weatherGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.weatherGridView.Location = new System.Drawing.Point(218, 23);
+            this.weatherGridView.Location = new System.Drawing.Point(234, 23);
             this.weatherGridView.Name = "weatherGridView";
             this.weatherGridView.RowHeadersWidth = 51;
             this.weatherGridView.RowTemplate.Height = 24;
-            this.weatherGridView.Size = new System.Drawing.Size(607, 129);
+            this.weatherGridView.Size = new System.Drawing.Size(591, 129);
             this.weatherGridView.TabIndex = 0;
             this.weatherGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.weatherGridView_CellContentClick);
             // 
             // chart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart.Legends.Add(legend2);
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart.Legends.Add(legend3);
             this.chart.Location = new System.Drawing.Point(32, 158);
             this.chart.Name = "chart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart.Series.Add(series2);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
             this.chart.Size = new System.Drawing.Size(793, 335);
             this.chart.TabIndex = 1;
             this.chart.Text = "chart";
--- WeatherCrimeAnalyzer/WeatherForm.cs
+++ WeatherCrimeAnalyzer/WeatherForm.cs
@@ -30,11 +30,7 @@ namespace WeatherCrimeAnalyzer
             {
                 string filePath = openFileDialog.FileName;
                 weatherList = WeatherService.LoadWeatherFromCsv(filePath);
-
-                // Заполнение DataGridView данными
                 weatherGridView.DataSource = weatherList;
-
-                // Отображение графика и статистики
                 PlotGraph();
                 ShowStats();
             }
@@ -45,33 +41,28 @@ namespace WeatherCrimeAnalyzer
         {
             chart.Series.Clear();
 
-            // Серия для минимальных температур (синим)
             var minSeries = new Series("Min")
             {
                 Color = System.Drawing.Color.Blue,
                 ChartType = SeriesChartType.Line
             };
 
-            // Серия для максимальных температур (красным)
             var maxSeries = new Series("Max")
             {
                 Color = System.Drawing.Color.Red,
                 ChartType = SeriesChartType.Line
             };
 
-            // Добавление точек для минимальных и максимальных температур
             for (int i = 0; i < weatherList.Count; i++)
             {
                 minSeries.Points.AddXY(i + 1, weatherList[i].MinTemp);
                 maxSeries.Points.AddXY(i + 1, weatherList[i].MaxTemp);
             }
 
-            // Добавляем серии на график
             chart.Series.Add(minSeries);
             chart.Series.Add(maxSeries);
         }
 
-        // Метод для отображения статистики (макс и мин перепад температур)
         private void ShowStats()
         {
             var maxDiff = WeatherAnalyzer.GetMaxTempDiffDay(weatherList);
@@ -87,26 +78,20 @@ namespace WeatherCrimeAnalyzer
             int n = int.Parse(nInput.Text);  // Пользователь вводит N для скользящей средней
             int futureDays = int.Parse(futureDaysInput.Text);  // Количество дней для прогноза
 
-            // Получаем список средних температур
             var avgTemps = weatherList.Select(d => d.AvgTemp).ToList();
 
-            // Получаем прогноз
             var forecast = WeatherAnalyzer.Forecast(avgTemps, n, futureDays);
 
-            // Серия для прогноза (зелёным)
-            var forecastSeries = new Series("Forecast")
+            var forecastSeries = new Series("Прогноз")
             {
                 Color = System.Drawing.Color.Green,
                 ChartType = SeriesChartType.Line
             };
 
-            // Добавляем данные прогноза на график
             for (int i = 0; i < forecast.Count; i++)
             {
                 forecastSeries.Points.AddXY(weatherList.Count + i + 1, forecast[i]);
             }
-
-            // Добавляем серию прогноза на график
             chart.Series.Add(forecastSeries);
         }
 
